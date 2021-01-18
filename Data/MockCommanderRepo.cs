9116ace35fe487ebe0c36fbe4ddb2cc228ba0031
Adolfo Lopez <adolfolopez88@gmail.com>
2021-01-17 22:59:00 -0300

    c# api commanders first commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Api.Models;
+using Commander.Data;
+
+namespace Api.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command> {
+                new Command { Id= 0, HowTo="Boil and egg", Line="boild water", Platform= "kettle" },
+                new Command { Id= 1, HowTo="cut bread", Line="boild water", Platform= "knife a choppong board" },
+                new Command { Id= 2, HowTo="Make a cup a tea", Line="boild water", Platform= "kettle" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id= 0, HowTo="Boil and egg", Line="boild water", Platform= "kettle" };
+        }
+    }
+}
